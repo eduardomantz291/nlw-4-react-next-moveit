a968de5ade59fbd7d22924ebaf1baf5c10239119
eduardomantz291 <eduardomantz2112@gmail.com>
2021-02-23 12:18:38 -0300

    estilo da barra de experiencia feita

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
   return (
-    <header className="experience-bar">
+    <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
         <div style={{ width: '50%' }} />
 
-        <span className="current-experience" style={{ left: '50%' }}>300px</span>
+        <span className={styles.currentExperience} style={{ left: '50%' }}>300px</span>
       </div>
       <span>600 xp</span>
     </header>
